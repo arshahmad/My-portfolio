6b64cc20a00027557f15317c5220df07ae9f58d5
arshahmad <arshahmadhdi@gmail.com>
2025-01-16 22:27:58 +0530

    experience years updated

--- src/App.tsx
+++ src/App.tsx
@@ -109,7 +109,7 @@ function App() {
           <div>
             <h3 className="text-3xl py-1 dark:text-white">About </h3>
             <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200">
-              Experienced and forward-thinking Software Engineer with over 2.5
+              Experienced and forward-thinking Software Engineer with over 3.5
               years of expertise in a diverse array of front-end
               technologies,including
               <span className="text-teal-500">
